a6e134befa429e6dfe39070e4391131e865eeff8
kanishk-00 <kanishkchugh1@gmail.com>
2023-12-13 01:53:33 +0530

    product details section dynamic filter done

--- ec/app/views.py
+++ ec/app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.views import View
+from . models import Product
+from django.db.models import Count
 
 # Create your views here.
 def home(req):
     return render(req, "app/home.html")
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
+        return render(request, "app/category.html", locals())
+
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request, "app/category.html", locals())
+
+class ProductDetail(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', locals())
