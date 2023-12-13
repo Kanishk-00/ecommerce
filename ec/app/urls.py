7518d7f3874ca58d3713863655a9a4e96410dffc
kanishk-00 <kanishkchugh1@gmail.com>
2023-12-13 23:37:41 +0530

    password changed functionality added

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm, MyPasswordResetForm
+from .forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm
 
 urlpatterns = [
     path('', views.home),
@@ -18,6 +18,8 @@ urlpatterns = [
     path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class = MyPasswordResetForm),
          name = 'password-reset'), 
     path('profile/', views.ProfileView.as_view(), name = 'profile'),
-    path('address/', views.address, name = 'address')
-
+    path('address/', views.address, name = 'address'), 
+    path('updateAddress/<int:pk>', views.updateAddress.as_view(), name = 'updateAddress'),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name = 'app/changepassword.html', form_class = MyPasswordChangeForm, success_url = '/passwordchangedone'), name = 'passwordchange'),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name = 'app/passwordchangedone.html'), name = 'passwordchangedone')
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- ec/app/views.py
+++ ec/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from . models import Product
+from django.shortcuts import redirect
 from django.db.models import Count
 from . forms import CustomerRegistrationForm , CustomerProfileForm
 from django.contrib import messages
@@ -80,3 +81,27 @@ class ProfileView(View):
 def address(request):
     add = Customer.objects.filter(user = request.user)
     return render(request, 'app/address.html', locals())
+
+
+class updateAddress(View):
+    def get(self, request, pk):
+        add = Customer.objects.get(pk = pk)
+        form = CustomerProfileForm(instance=add)
+        return render(request, 'app/updateAddress.html', locals())
+    def post(self, request, pk):
+        form = CustomerProfileForm(request.POST) 
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request, "Congratulations! Profile Updated Successfully")
+        else:
+            messages.warning(request, "Invalid Input Data")
+        return redirect("address")  
+    
+    
